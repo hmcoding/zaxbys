9ffd6013e3378750b6da852edfa2b082600038c9
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-04-16 17:32:10 -0400

    Create changedshell_error.h

---
+++ src/include/changedshell_error.h
@@ -0,0 +1,39 @@
+#ifndef SHELL_ERROR_H__
+#define SHELL_ERROR_H__
+
+//parsing errors
+void error_dangling_quote();
+
+// set of open/close errors
+void error_open_already(char *fNames);
+void error_open_no_file(char *fNames);
+void error_open_directory(char *directory);
+void error_open_bad_param(char *param);
+void error_close_directory(char *directory);
+void error_not_open(char *fNames);
+
+// create errors
+void error_used_file(char *nameFile);
+void error_specify_file_and_mode(char *command);
+void error_specify_file(char *command);
+void error_specify_directory(char *command);
+void error_no_more_space();
+
+// set of cd errors
+void error_cd_file(char *fNames);
+void error_cd_not_here(char *directory);
+
+// set of read/write errors
+void error_specify_file_pos_size(char *command);
+void error_specify_file_pos_size_str(char *command);
+void error_not_readable(char *fNames);
+void error_beyond_EOF(unsigned int location, unsigned int size, unsigned int sizeFile);
+void error_not_writeable(char *fNames);
+void error_too_large(unsigned int location, unsigned int size);
+
+void error_bad_directory(char *fNames);
+void error_not_empty(char *directory);
+void error_file_or_directory_exists(char *fNames);
+void error_remove_directory(char *directory);
+
+#endif
