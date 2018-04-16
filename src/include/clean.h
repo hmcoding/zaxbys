7ded11da787d4841373f04549925c346b884f213
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-04-16 14:37:28 -0400

    Update clean.h

--- src/include/clean.h
+++ src/include/clean.h
@@ -1,8 +1,8 @@
 #ifndef CLEAN_H__
 #define CLEAN_H__
 
-// Simple function for cleaning up resources
-void clean_up_loop(char *cmd_line, char **cmd_args);
-void clean_up_globals(void);
+
+void loopClean(char *cmd_line, char **cmd_args);
+void globClean(void);
 
 #endif
