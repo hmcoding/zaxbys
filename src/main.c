dded0f51717cff42eb09ab80b8bb79bc7f6c9a12
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-04-23 16:07:21 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #include "setup.h"
 #include "parse.h"
 #include "execute.h"
@@ -8,20 +9,21 @@
 #include "tools.h"
 #include "file_types.h"
 
-int endianness;
-FILE *fat32_img;
-struct fat32_info img_info;
-unsigned int first_data_sec;
-unsigned int first_root_sec;
-char *current_directory;
-unsigned int cur_dir_sec;
+
+int endianVar;
+FILE *fatImage;
+struct fatData imageData;
+unsigned int dataSec;
+unsigned int rootSec;
+char *thisDir;
+unsigned int thisDirSec;
 
 
 
 int main(int argc, char *argv[]) {
-	char *cmd_line;
-	char **cmd_args;
-	int status;
+	char *progLine;
+	char **progArgs;
+	int program;
 
 	if (argc < 2) {
 		printf("USAGE: %s <FAT32 img>\n", argv[0]);
@@ -31,17 +33,17 @@ int main(int argc, char *argv[]) {
 	if (!setup(argv[1], argv[0])) {
 		return 0;
 	}
-	status = 1;
-	while (status) {
+	program = 1;
+	while (program) {
 
 		displayPrompt();
-		if((cmd_line = readIn()) == NULL) {
+		if((progLine = readIn()) == NULL) {
 			fprintf(stderr, "Bad read, try again\n");
 			continue;
 		}
-		cmd_args = parse(cmd_line);
-		status = userCmd(cmd_args);
-		loopClean(cmd_line, cmd_args);
+		progArgs = parse(progLine);
+		program = userCmd(progArgs);
+		loopClean(progLine, progArgs);
 	}
 	globClean();
 
