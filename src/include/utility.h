02ef6561664c84ab8ce4ca5dfb1712f5dfb8c66e
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-04-23 21:43:00 -0400

    Create utility.h

---
+++ src/include/utility.h
@@ -0,0 +1,31 @@
+#ifndef UTILITY_H__
+#define UTILITY_H__
+
+union dirEntry;
+
+void loopClean(char *progLine, char **progArgs);
+void globClean(void);
+void error(int type, char* print);
+
+int displayDir(unsigned int dirClus);
+int changeDirClus(union dirEntry *ptr);
+int changeCurDir(union dirEntry *ptr);
+unsigned int lookupNextToLastSlash();
+
+int userCmd(char **progArgs);
+int infoCmd();
+int openCmd(char **progArgs);
+int closeCmd(char **progArgs);
+int createCmd(char **progArgs);
+int rmCmd(char **progArgs);
+int sizeCmd(char **progArgs);
+int readCmd(char **progArgs);
+int writeCmd(char **progArgs);
+int exitCmd(void);
+int cdCmd(char **progArgs);
+int lsCmd(char **progArgs);
+int mkdirCmd(char **progArgs);
+int rmdirCmd(char **progArgs);
+
+
+#endif
