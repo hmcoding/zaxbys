a133d7253847b7f1ded2c58bf294195980332308
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-04-29 13:48:33 -0400

    Update myUtility.h

--- src/include/myUtility.h
+++ src/include/myUtility.h
@@ -6,26 +6,24 @@ union dirEntry;
 void loopClean(char *progLine, char **progArgs);
 void globClean(void);
 void error(int type, char* print);
-
 int displayDir(unsigned int dirClus);
 int changeDirClus(union dirEntry *ptr);
 int changeCurDir(union dirEntry *ptr);
 unsigned int lookupNextToLastSlash();
-
 int userCmd(char **progArgs);
+int exitCmd(void);
 int infoCmd();
+int lsCmd(char **progArgs);
+int cdCmd(char **progArgs);
+int sizeCmd(char **progArgs);
+int createCmd(char **progArgs);
+int mkdirCmd(char **progArgs);
+int rmCmd(char **progArgs);
+int rmdirCmd(char **progArgs);
 int openCmd(char **progArgs);
 int closeCmd(char **progArgs);
-int createCmd(char **progArgs);
-int rmCmd(char **progArgs);
-int sizeCmd(char **progArgs);
 int readCmd(char **progArgs);
 int writeCmd(char **progArgs);
-int exitCmd(void);
-int cdCmd(char **progArgs);
-int lsCmd(char **progArgs);
-int mkdirCmd(char **progArgs);
-int rmdirCmd(char **progArgs);
 
 
 #endif
