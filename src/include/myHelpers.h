822722f3106fe5f231d1ce8c9a29f6249aa2fe04
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-04-29 13:47:53 -0400

    Update myHelpers.h

--- src/include/myHelpers.h
+++ src/include/myHelpers.h
@@ -7,44 +7,34 @@
 
 union dirEntry;
 
-int rChar(void *ptr, long pos, size_t use);
-int wChar(void *ptr, long pos, size_t use);
-
-unsigned short retLoVal(unsigned int clus);
-unsigned short retHiVal(unsigned int clus);
-
-
-unsigned int *readUnInt(unsigned int *ptr, long pos);
-unsigned int *writeUnInt(unsigned int *ptr, long pos);
-unsigned short *readUnSh(unsigned short *ptr, long pos);
-unsigned short *writeUnSh(unsigned short *ptr, long pos);
-unsigned char *readUnCh(unsigned char *ptr, long pos);
-unsigned int switch32(unsigned int val);
 unsigned short switch16(unsigned short val);
-
+unsigned int switch32(unsigned int val);
 int endianSee(void);
-
-unsigned int retSecClus(unsigned int clus);
+int chainEnd(unsigned int clus);
+int rChar(void *ptr, long pos, size_t use);
+unsigned int *readUnInt(unsigned int *ptr, long pos);
+unsigned short *readUnSh(unsigned short *ptr, long pos);
+unsigned char *readUnCh(unsigned char *ptr, long pos);
+int wChar(void *ptr, long pos, size_t use);
+unsigned int *writeUnInt(unsigned int *ptr, long pos);
+unsigned short *writeUnSh(unsigned short *ptr, long pos);
+int changeFats(unsigned int fileClus, unsigned int value);
+int expClus(unsigned int oldClus1);
 unsigned long retFatClusPos(unsigned int clus, unsigned int fat);
+unsigned int retSecClus(unsigned int clus);
 unsigned int retFatNextClus(unsigned int clus);
 unsigned int retFatNextClus_true(unsigned int clus);
-int chainEnd(unsigned int clus);
-int changeFats(unsigned int fileClus, unsigned int value);
-
-int shortLow(char fNames[12], char shNames[11]);
-int toShortFile(char fNames[12], char shNames[11]);
-int lookupFile(char *fNames, unsigned int dirClus, union dirEntry *ptr, unsigned int *ptrClus, unsigned int *ptrOff);
-unsigned int retFileClus(union dirEntry *ptr);
 unsigned short retHiVal(unsigned int clus);
 unsigned short retLoVal(unsigned int clus);
-int emptyDir(union dirEntry *dir);
-
 unsigned short retTime(void);
 unsigned short int retDate(void);
-
+unsigned int retFileClus(union dirEntry *ptr);
+int lookupFile(char *fNames, unsigned int dirClus, union dirEntry *ptr, unsigned int *ptrClus, unsigned int *ptrOff);
 unsigned int lookupOpenClus(void);
 unsigned int lookupOpenDirEntry(unsigned int dirClus, union dirEntry *ptr, unsigned int *ptrClus, unsigned int *ptrOff);
-
+int emptyDir(union dirEntry *dir);
+int shortLow(char fNames[12], char shNames[11]);
+int toShortFile(char fNames[12], char shNames[11]);
 int setEntryNull(unsigned int dirClus, unsigned int entryDig);
-int expClus(unsigned int oldClus1);
+
 #endif
